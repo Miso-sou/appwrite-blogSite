140c558e062cbca726616c8cb762b37bce3ed584
Miso-sou <work.wadhwani@gmail.com>
2025-09-01 19:02:30 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APP_APPWRITE_URL);
+  
+
+  return (
+    <>
+      <h1>blog site</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/appwrite/auth.js
@@ -0,0 +1,43 @@
+import conf from '../config/appwriteConfig.js';
+import { Client, Account, ID } from 'appwrite';
+
+// Initialize the Appwrite client with help of classes instead of direct functions so that when creating a registration page they can just create a new instance of AuthService with the class object.
+
+export class AuthService {
+    Client = new Client();
+
+    constructor() {
+        this.Client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.Account = new Account(this.Client);
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount =   await this.Account.create(ID.unique(), email, password, name);
+
+            if (userAccount) {
+                // call another menthod
+                return userAccount;
+            } else {
+                throw new Error('Account creation failed');
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email, password}){
+        try {
+            return await this.Account.createEmailPasswordSession(email, password);
+
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+const authService = new AuthService()
+
+export default AuthService();
